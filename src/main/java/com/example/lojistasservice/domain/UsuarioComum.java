f53806abe91987d1bfd5703b4f39f06c1331df5c
Guilherme Ferreira <ferreiragui.dev@gmail.com>
2023-04-07 11:43:37 -0300

    usuário entity

---
+++ src/main/java/com/example/lojistasservice/domain/UsuarioComum.java
@@ -0,0 +1,38 @@
+package com.example.lojistasservice.domain;
+
+
+import com.example.lojistasservice.domain.enums.TiposUsuarios;
+import jakarta.persistence.*;
+import lombok.*;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+
+
+@Builder
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class UsuarioComum {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String nomeCompleto;
+
+    @Column(unique = true)
+    private String cpfCnpj;
+
+    @Column(unique = true)
+    private String email;
+
+    private double saldo;
+
+    private Integer desconto;
+
+    @Enumerated(EnumType.STRING)
+    private TiposUsuarios  tipoUsuario;
+
+}
